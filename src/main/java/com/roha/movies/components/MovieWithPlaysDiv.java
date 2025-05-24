d929853b52ccc0fe30023a7984220c1cb25ddb50
ronald haring <email@ronaldharing.com>
2025-05-24 12:25:46 +0200

    card not reactive enough

--- src/main/java/com/roha/movies/components/MovieWithPlaysDiv.java
+++ src/main/java/com/roha/movies/components/MovieWithPlaysDiv.java
@@ -4,45 +4,72 @@ import com.roha.movies.domain.Movie;
 import com.roha.movies.domain.MovieDTO;
 import com.roha.movies.fetcher.Initializer;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.card.Card;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 import java.io.IOException;
 
-public class MovieWithPlaysDiv extends Div {
+public class MovieWithPlaysDiv extends Card {
     private Movie movie;
     private Initializer initializer;
+
     public MovieWithPlaysDiv(Initializer initializer, Movie movie) {
         super();
         this.movie = movie;
         this.initializer = initializer;
-        HorizontalLayout horizontalLayout = new HorizontalLayout();
-        VerticalLayout movieOverview = new VerticalLayout();
-        movieOverview.add(new H3(movie.title()+" ("+movie.rating()+")"));
-        movieOverview.add(new Image(movie.imageHref(), "poster"));
-        horizontalLayout.add(movieOverview);
-        VerticalLayout load = new VerticalLayout();
-        Button loadButton = new Button("Load");
-        Div movieContent = new Div();
-        movieContent.setVisible(false);
-        loadButton.addClickListener(e -> {
-                    Movie movie1 = null;
-                    try {
-                        movie1 = initializer.loadMovie(movie.asDTO());
-                        movieContent.setText(movie1.content());
-                        loadButton.setVisible(false);
-                    } catch (IOException ex) {
-                        movieContent.setText("Could not load content");
-                    }
-                    movieContent.setVisible(true);
-                });
-        load.add(loadButton);
-        load.add(movieContent);
-        horizontalLayout.add(load);
-        add(horizontalLayout);
+        setTitle(movie.title() + " - " + movie.rating());
+        Paragraph movieContent = new Paragraph();
+        movieContent.setText("");
+        Image media = new Image(movie.imageHref(), movie.title());
+        Span span = new Span(media);
+        span.addClickListener(event -> {
+            Movie movie1 = null;
+            try {
+                movie1 = initializer.loadMovie(movie.asDTO());
+                movieContent.setText(movie1.content());
+            } catch (IOException ex) {
+                movieContent.setText("Could not load content");
+            }
+            ;
+        });
+        this.setMedia(span);
+        this.add(movieContent);
+        Button skipButton = new Button("Skip");
+        skipButton.addClickListener(event -> {
+            try {
+                initializer.getMoviesFetcher().addSkipped(movie.asDTO());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+        Button seenButton = new Button("Seen");
+        skipButton.addClickListener(event -> {
+            try {
+                initializer.getMoviesFetcher().addSeen(movie.asDTO());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+        Button wantedButton = new Button("Yessss");
+        skipButton.addClickListener(event -> {
+            try {
+                initializer.getMoviesFetcher().addSeen(movie.asDTO());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+        Button resetButton = new Button("Reset");
+        skipButton.addClickListener(event -> {
+            try {
+                initializer.getMoviesFetcher().reset(movie.asDTO());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+        HorizontalLayout buttons = new HorizontalLayout(FlexComponent.Alignment.START,skipButton, seenButton, wantedButton, resetButton);
+        setSubtitle(buttons);
     }
-
 }
